d52f25bd66a254436e041b53dd938830c1003960
marcdumon <marcdumon@msn.com>
2019-12-27 00:32:59 +0100

    Commit after branching

--- test.py
+++ test.py
@@ -6,12 +6,13 @@
 from ignite.engine import create_supervised_trainer, create_supervised_evaluator, Events
 from ignite.metrics import Accuracy, Loss
 from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
 from torchvision.models import resnet18
 import torch.nn as nn
 from torchvision.transforms import transforms
 from tqdm import tqdm
 
-from data_process.standard_datasets import MNIST_Dataset
+# from data_process.standard_datasets import MNIST_Dataset
 
 # !/usr/bin/env python
 
@@ -25,22 +26,29 @@ import numpy as np
 import random
 import torch as th
 
+from data_process import MNIST_Dataset
 from data_process.utils import stratified_split_train_valid, random_split_train_valid
+from machine import xxx, Model
+from models.standard_models import MNSIT_Simple
 from visualization.utils import show_mpl_grid
+import configuration
+
+
 
 seed = 42
 random.seed(seed)
 np.random.seed(seed)
 th.manual_seed(seed)
-# th.backends.cudnn.deterministic = True
-# th.backends.cudnn.benchmark = False
-bs = 8 * 512
+th.backends.cudnn.deterministic = True
+th.backends.cudnn.benchmark = False
+bs = 8 * 64
+tb_logdir = '/media/md/Development/My_Projects/0_ml_project_template.v1/tensorboard/'
 
 transform = transforms.Compose([transforms.ToTensor(),
-                                transforms.Normalize((0.1307,), (0.3081,))])
+                                transforms.Normalize((0.1307,), (0.3081,))
+                                ])
 mnist_ds = MNIST_Dataset(sample=False, transform=transform)
 train, valid = random_split_train_valid(dataset=mnist_ds, valid_frac=.2)
-print(len(train), len(valid))
 
 train_loader = DataLoader(train, batch_size=bs, num_workers=8, shuffle=True)
 val_loader = DataLoader(valid, batch_size=bs, num_workers=8, shuffle=True)
@@ -50,49 +58,17 @@ one_valid_batch = next(iter(val_loader))  # => list []
 # targets = list(one_valid_batch[1].numpy())
 # show_mpl_grid(images, targets)
 
-model = resnet18(pretrained=True, progress=True)
-model.conv1 = nn.Conv2d(1, 64, kernel_size=7, stride=2, padding=3, bias=False)
 
-model.fc = nn.Sequential(nn.Linear(512, 512), nn.ReLU(), nn.Linear(512, 10))
+# model = resnet18(pretrained=True, progress=True)
+# model.conv1 = nn.Conv2d(1, 64, kernel_size=7, stride=2, padding=3, bias=False)
+# model.fc = nn.Sequential(nn.Linear(512, 512), nn.ReLU(), nn.Linear(512, 10))
+# model = MNSIT_Simple()
+model = Model()
+
 # optimizer = th.optim.SGD(model.parameters(), lr=0.01, momentum=0.8)
-optimizer = th.optim.Adam(model.fc.parameters(), lr=1e-4)
+# optimizer = th.optim.Adam(model.fc.parameters(), lr=1e-4)
+optimizer = th.optim.Adam(model.parameters(), lr=3e-3)
 loss = th.nn.NLLLoss()
 
-trainer = create_supervised_trainer(model, optimizer, loss, device='cuda')
-evaluator = create_supervised_evaluator(model, metrics={'accuracy': Accuracy(), 'nll': Loss(loss)}, device='cuda')
-
-desc = "ITERATION - loss: {:.2f}"
-pbar = tqdm(initial=0, leave=False, total=len(train_loader),
-            desc=desc.format(0))
-
-
-@trainer.on(Events.ITERATION_COMPLETED(every=1))
-def log_training_loss(engine):
-    pbar.desc = desc.format(engine.state.output)
-    pbar.update(1)
-
-
-# def log_training_loss(trainer):
-# print("Epoch[{}] Loss: {:.2f}".format(trainer.state.epoch, trainer.state.output))
-
-
-@trainer.on(Events.EPOCH_COMPLETED)
-def log_training_results(engine):
-    pbar.refresh()
-    evaluator.run(train_loader)
-    metrics = evaluator.state.metrics
-    avg_accuracy = metrics['accuracy']
-    avg_nll = metrics['nll']
-    tqdm.write("\nTrain Results - Epoch: {}  Avg accuracy: {:.2f} Avg loss: {:.2f}"
-               .format(engine.state.epoch, avg_accuracy, avg_nll)               )
-
-
-@trainer.on(Events.EPOCH_COMPLETED)
-def log_validation_results(trainer):
-    evaluator.run(val_loader)
-    metrics = evaluator.state.metrics
-    print("Valid Results - Epoch: {}  Avg accuracy: {:.2f} Avg loss: {:.2f}"
-          .format(trainer.state.epoch, metrics['accuracy'], metrics['nll']))
-
-
+trainer = xxx(model, train_loader, val_loader, optimizer, loss)
 trainer.run(train_loader, max_epochs=100)
