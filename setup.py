5b033567350aaa46e43d4e900270c5a725cb3aca
marcdumon <marcdumon@msn.com>
2020-01-06 11:06:26 +0100

    Make installable package

---
+++ setup.py
@@ -0,0 +1,19 @@
+# --------------------------------------------------------------------------------------------------------
+# 2020/01/06
+# src - setup.py
+# md
+# --------------------------------------------------------------------------------------------------------
+
+from setuptools import setup
+
+setup(
+    name='ptolemaios',
+    version='0.0.1',
+    description='My private package from private github repo',
+    url='git@github.com:https://github.com/marcdumon/ai_template.git',
+    author='Raphael Schubert',
+    author_email='raphael.schubert@digitalbankscorp.com',
+    license='unlicense',
+    packages=['ptolemaios'],
+    zip_safe=False
+)
